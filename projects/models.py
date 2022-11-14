e12067b944a186a38c0d572338b5761881bccbef
Arseny <>
2022-11-14 21:35:22 +0300

    Design of project page

--- projects/models.py
+++ projects/models.py
@@ -8,9 +8,24 @@ class Project(models.Model):
     short_description = models.TextField(default="")
     description = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
+    youtube_link = models.CharField(max_length=200, default='')
+    github_link = models.CharField(max_length=200, default='')
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+'''
+class About(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    photo = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=200, unique=True)
+    experience = models.TextField(default="")
+    skills = models.TextField(default="")
+    linkedin_link = models.CharField(default='')
+    github_link = models.CharField(default='')
+
+    def __str__(self):
+        return self.name
+'''
